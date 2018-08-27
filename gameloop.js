7e15154d7af2b1b498f349d26500637aeafea91d
Josh Mason <joshua.thomas.mason@ibm.com>
2018-08-27 00:15:41 -0400

    Starting to layout the UI

--- gameloop.js
+++ gameloop.js
@@ -7,7 +7,25 @@ let MyObject = function () {
 }
 var o = new MyObject();
 setInterval(onTimerTick, 40); // 33 milliseconds = ~ 30 frames per sec
-document.getElementById("test").innerHTML = o.type;
+var num = 25;
+let constructTable = function (){
+	var str = "";
+	for (i = 0; i < num; i++) { 
+		str += "<tr>";
+		for (j = 0; j < num; j++) { 
+			str += "<td>x</td>";
+		}
+		str += "</tr>";
+	}
+	return str;
+}
+let createNotification = function(notification){
+	var str= '<div class="alert alert-success fade in">  <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>       <p><strong>Alert!</strong></p> '+      notification +'     </div>';
+	return str;
+}
+document.getElementById("mappane").innerHTML = '<table cellspacing="0" style="width:100%">' + constructTable() + '</table>';
+
+
 function onTimerTick() {
     t=t+1;
 	//document.getElementById("test").innerHTML = t;
@@ -15,7 +33,8 @@ function onTimerTick() {
 
 
 document.onkeypress=function(e){
-    document.getElementById("test").innerHTML = String.fromCharCode(e.keyCode);
+    //document.getElementById("test").innerHTML = String.fromCharCode(e.keyCode);
+	document.getElementById("notificationtab").innerHTML = createNotification(String.fromCharCode(e.keyCode));
 }
 
 
